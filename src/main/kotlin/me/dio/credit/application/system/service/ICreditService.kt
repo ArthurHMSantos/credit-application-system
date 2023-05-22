ac572aa81c0396f98aeb23dfd2d7a787bfef7d2a
Arthur Henrique <Arthur.profissional20@gmail.com>
2023-05-22 15:55:47 -0300

    CustomerService

---
+++ src/main/kotlin/me/dio/credit/application/system/service/ICreditService.kt
@@ -0,0 +1,10 @@
+package me.dio.credit.application.system.service
+
+import me.dio.credit.application.system.entities.Credit
+import java.util.UUID
+
+interface ICreditService {
+    fun register(credit: Credit ): Credit
+    fun findAllByCustomer(customerId: Long): List<Credit>
+    fun findByCreditCode(creditCode: UUID): Credit
+}
---
+++ src/main/kotlin/me/dio/credit/application/system/service/implement/CustomerService.kt
@@ -0,0 +1,23 @@
+package me.dio.credit.application.system.service.implement
+
+import me.dio.credit.application.system.entities.Customer
+import me.dio.credit.application.system.repository.CustomerRepo
+import me.dio.credit.application.system.service.ICustomerService
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService(
+    private val customerRepo: CustomerRepo
+): ICustomerService {
+    override fun register(customer: Customer): Customer =
+        this.customerRepo.save(customer)
+
+    override fun findById(id: Long): Customer =
+        this.customerRepo.findById(id).orElseThrow {
+            throw RuntimeException("Id $id not found")
+    }
+
+    override fun delete(id: Long) =
+        this.customerRepo.deleteById(id)
+
+}
